812f645d87fef965f2f1dd5b750e2bed04c5e8da
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-12-08 13:39:49 +0100

    v_package

--- Ejercicios/Ejercicio6.sql
+++ Ejercicios/Ejercicio6.sql
@@ -1,1 +1,25 @@
+select * from propietarios;
+select * from HISTORIAL_CARGOS;
+select * from comunidades;
+describe HISTORIAL_CARGOS;
+
+
+--Paquete de variables
+CREATE OR REPLACE PACKAGE vcargos
+AS
+
+TYPE columnascargos IS RECORD
+(
+    CODCOMUNIDAD VARCHAR2(8),
+    DNI VARCHAR2(9),
+    FECHA_INICIO DATE,
+    FECHA_FIN DATE
+);
+
+TYPE tablacargos IS TABLE OF columnascargos INDEX BY BINARY_INTEGER;
+
+info_historial_cargos tablacargos;
+
+END;
+/
 
