1ed6388a5d4a7c5eb027983b040c7087606a0425
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-12-08 13:40:23 +0100

    trigger rellenar package

--- Ejercicios/Ejercicio6.sql
+++ Ejercicios/Ejercicio6.sql
@@ -23,3 +23,20 @@ info_historial_cargos tablacargos;
 END;
 /
 
+--Trigger para rellenar las variables del paquete
+CREATE OR REPLACE TRIGGER rellenarcolumnascargos
+BEFORE INSERT or UPDATE on HISTORIAL_CARGOS
+DECLARE
+    cursor info_cargos IS select CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN from HISTORIAL_CARGOS;
+    INDICE NUMBER:=0;
+BEGIN
+    FOR v_cur in info_cargos LOOP
+        vcargos.info_historial_cargos(INDICE).CODCOMUNIDAD := v_cur.CODCOMUNIDAD;
+        vcargos.info_historial_cargos(INDICE).DNI := v_cur.DNI;
+        vcargos.info_historial_cargos(INDICE).FECHA_INICIO := v_cur.FECHA_INICIO;
+        vcargos.info_historial_cargos(INDICE).FECHA_FIN := v_cur.FECHA_FIN;
+        INDICE := INDICE + 1;
+    END LOOP;
+END;
+/
+
