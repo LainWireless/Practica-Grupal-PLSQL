39a01199fbfa9b1a082f90eec97507bc2e57d169
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-12-10 14:23:20 +0100

    Package y trigger para rellenar variables

--- Ejercicios/Ejercicio7.sql
+++ Ejercicios/Ejercicio7.sql
@@ -1,1 +1,36 @@
+--Paquete de variables
+CREATE OR REPLACE PACKAGE vmandatos
+AS
+
+TYPE columnasmandatos IS RECORD
+(
+    NUMCOLEGIADO VARCHAR2(5),
+    CODCOMUNIDAD VARCHAR2(8),
+    FECHA_INICIO DATE,
+    FECHA_FINAL DATE
+);
+
+TYPE tablamandatos IS TABLE OF columnasmandatos INDEX BY BINARY_INTEGER;
+
+info_contratos_mandatos tablamandatos;
+
+END;
+/
+
+--Trigger para rellenar las variables del paquete
+CREATE OR REPLACE TRIGGER rellenarcolumnasmandatos
+BEFORE INSERT or UPDATE on CONTRATOS_DE_MANDATO
+DECLARE
+    cursor info_mandatos IS select NUMCOLEGIADO,CODCOMUNIDAD,FECHA_INICIO,FECHA_FINAL from CONTRATOS_DE_MANDATO;
+    INDICE NUMBER:=0;
+BEGIN
+    FOR v_cur in info_mandatos LOOP
+        vmandatos.info_contratos_mandatos(INDICE).NUMCOLEGIADO := v_cur.NUMCOLEGIADO;
+        vmandatos.info_contratos_mandatos(INDICE).CODCOMUNIDAD := v_cur.CODCOMUNIDAD;
+        vmandatos.info_contratos_mandatos(INDICE).FECHA_INICIO := v_cur.FECHA_INICIO;
+        vmandatos.info_contratos_mandatos(INDICE).FECHA_FINAL := v_cur.FECHA_FINAL;
+        INDICE := INDICE + 1;
+    END LOOP;
+END;
+/
 
