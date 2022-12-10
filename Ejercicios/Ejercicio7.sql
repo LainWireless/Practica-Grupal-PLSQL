52cd2580bd088f6e572b026f4ccd0bddaca99b53
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-12-10 17:34:15 +0100

    trigger principal, función para verificar fechas

--- Ejercicios/Ejercicio7.sql
+++ Ejercicios/Ejercicio7.sql
@@ -34,3 +34,44 @@ BEGIN
 END;
 /
 
+--Trigger para controlar los contratos de mandatos
+CREATE OR REPLACE TRIGGER Control_mandatos
+BEFORE INSERT OR UPDATE ON CONTRATOS_DE_MANDATO
+FOR EACH ROW
+DECLARE
+    INDICE2 NUMBER:=0;
+    v_rfecha NUMBER;
+BEGIN
+    FOR v_cur in vmandatos.info_contratos_mandatos.FIRST.. vmandatos.info_contratos_mandatos.LAST LOOP
+        select comprobar_fecha_mandatos(:NEW.FECHA_INICIO,0) into v_rfecha from dual;
+
+        IF :NEW.NUMCOLEGIADO = vcargos.info_historial_cargos(INDICE2).NUMCOLEGIADO AND :NEW.CODCOMUNIDAD = vcargos.info_historial_cargos(INDICE2).CODCOMUNIDAD THEN
+            IF :NEW.FECHA_INICIO >= vcargos.info_historial_cargos(INDICE2).FECHA_FINAL AND :NEW.FECHA_FIN <= vcargos.info_historial_cargos(INDICE2).FECHA_FIN THEN
+                RAISE_APPLICATION_ERROR(-20001, 'Los Administradores solo pueden gestionar un máximo de 4 comunidades simultáneamente');
+            END IF; 
+	    END IF;
+        INDICE2 := INDICE2 + 1;
+    END LOOP;
+
+
+END;
+/
+
+--Función para comprobar rangos de fechas
+create or replace FUNCTION comprobar_fecha_mandatos(p_fecha in date, INDICE2 in NUMBER)
+RETURN NUMBER
+IS
+	v_rfecha NUMBER;
+BEGIN
+    select 1 into v_rfecha from dual 
+    WHERE p_fecha BETWEEN vmandatos.info_contratos_mandatos(INDICE2).fecha_inicio AND
+    vmandatos.info_contratos_mandatos(INDICE2).fecha_final;
+	if v_rfecha = 1 THEN
+        RETURN v_rfecha;
+    ELSE
+        v_rfecha := 0;
+        RETURN v_rfecha;
+    END IF;
+END;
+/
+
