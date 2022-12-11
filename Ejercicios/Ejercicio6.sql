e3733cc5bd8e11950594cf9e1337c83943418aa9
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-12-11 17:00:33 +0100

    Posibles Errores (Funciona)

--- Ejercicios/Ejercicio6.sql
+++ Ejercicios/Ejercicio6.sql
@@ -40,26 +40,48 @@ BEGIN
 END;
 /
 
+--Función para comprobar rangos de fechas
+create or replace FUNCTION comprobar_fecha_cargos(p_fecha in date,INDICE2 in NUMBER,p_dni in VARCHAR2,p_codcomunidad in VARCHAR2)
+RETURN NUMBER
+IS
+	v_rfecha NUMBER;
+BEGIN
+    select 1 into v_rfecha from dual 
+    WHERE p_fecha BETWEEN vcargos.info_historial_cargos(INDICE2).fecha_inicio AND
+    vcargos.info_historial_cargos(INDICE2).fecha_fin AND vcargos.info_historial_cargos(INDICE2).dni = p_dni AND vcargos.info_historial_cargos(INDICE2).CODCOMUNIDAD = p_codcomunidad;
+	if v_rfecha = 1 THEN
+        RETURN v_rfecha;
+    ELSE
+        v_rfecha := 0;
+        RETURN v_rfecha;
+    END IF;
+END;
+/
+
+
 --Trigger para controlar los cargos
 CREATE OR REPLACE TRIGGER Control_cargos
 BEFORE INSERT OR UPDATE ON HISTORIAL_CARGOS
 FOR EACH ROW
 DECLARE
     INDICE2 NUMBER:=0;
+    v_rfecha NUMBER;
+    contador NUMBER:=0;
 BEGIN
-	IF :NEW.CODCOMUNIDAD = vcargos.info_historial_cargos(INDICE2).CODCOMUNIDAD AND :NEW.DNI = vcargos.info_historial_cargos(INDICE2).DNI THEN
-        IF :NEW.FECHA_INICIO >= vcargos.info_historial_cargos(INDICE2).FECHA_INICIO AND :NEW.FECHA_FIN <= vcargos.info_historial_cargos(INDICE2).FECHA_FIN THEN
-            RAISE_APPLICATION_ERROR(-20001, 'Los propietarios solo pueden ocupar un cargo en la misma comunidad');
+    FOR v_cur in vcargos.info_historial_cargos.FIRST.. vcargos.info_historial_cargos.LAST LOOP
+        select comprobar_fecha_cargos(:NEW.FECHA_INICIO,INDICE2,:NEW.DNI,:NEW.CODCOMUNIDAD) into v_rfecha from dual;
+        IF v_rfecha = 1 THEN
+            contador := contador + 1;
         END IF; 
---    ELSIF :NEW.CODCOMUNIDAD = :OLD.CODCOMUNIDAD AND :NEW.DNI = :OLD.DNI THEN
---        IF :NEW.FECHA_INICIO > :OLD.FECHA_INICIO AND :NEW.FECHA_FIN < :OLD.FECHA_FIN THEN
---            RAISE_APPLICATION_ERROR(-20001, 'Los propietarios solo pueden ocupar un cargo en la misma comunidad');
---        END IF;
-	END IF;
-    INDICE2 := INDICE2 + 1;
+        IF contador = 1 THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Los propietarios solo pueden ocupar un cargo en la misma comunidad');
+	    END IF;
+        INDICE2 := INDICE2 + 1;
+    END LOOP;
+    contador := 0;
+    vcargos.info_historial_cargos.DELETE;
 END;
 /
-
 
 --Pruebas
 
@@ -68,6 +90,12 @@ VALUES('Presidente','AAAA1','49027387N',
 TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'));
 INSERT INTO HISTORIAL_CARGOS
 VALUES('vocal','AAAA1','49027387N',
+TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'));
+INSERT INTO HISTORIAL_CARGOS
+VALUES('Presidente','AAAA2','49027387N',
+TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'));
+INSERT INTO HISTORIAL_CARGOS
+VALUES('vocal','AAAA3','49027387N',
 TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'));
 
 delete from HISTORIAL_CARGOS where FECHA_INICIO = TO_DATE('2018/01/15','YYYY/MM/DD');
