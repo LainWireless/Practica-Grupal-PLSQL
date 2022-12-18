bdc39adfcc74ff91a99fdd0be55fe587198e9905
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-12-18 23:07:43 +0100

    Error-tablas-vacías (Funciona)

--- Ejercicios/Ejercicio6.sql
+++ Ejercicios/Ejercicio6.sql
@@ -1,9 +1,3 @@
-select * from propietarios;
-select * from HISTORIAL_CARGOS;
-select * from comunidades;
-describe HISTORIAL_CARGOS;
-
-
 --Paquete de variables
 CREATE OR REPLACE PACKAGE vcargos
 AS
@@ -20,6 +14,8 @@ TYPE tablacargos IS TABLE OF columnascargos INDEX BY BINARY_INTEGER;
 
 info_historial_cargos tablacargos;
 
+v_status_table NUMBER;
+
 END;
 /
 
@@ -30,15 +26,14 @@ DECLARE
     cursor info_cargos IS select CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN from HISTORIAL_CARGOS;
     INDICE NUMBER:=0;
 BEGIN
-    IF info_cargos%found then
-        FOR v_cur in info_cargos LOOP
-            vcargos.info_historial_cargos(INDICE).CODCOMUNIDAD := v_cur.CODCOMUNIDAD;
-            vcargos.info_historial_cargos(INDICE).DNI := v_cur.DNI;
-            vcargos.info_historial_cargos(INDICE).FECHA_INICIO := v_cur.FECHA_INICIO;
-            vcargos.info_historial_cargos(INDICE).FECHA_FIN := v_cur.FECHA_FIN;
-            INDICE := INDICE + 1;
-        END LOOP;
-    end if;
+    FOR v_cur in info_cargos LOOP
+        vcargos.info_historial_cargos(INDICE).CODCOMUNIDAD := v_cur.CODCOMUNIDAD;
+        vcargos.info_historial_cargos(INDICE).DNI := v_cur.DNI;
+        vcargos.info_historial_cargos(INDICE).FECHA_INICIO := v_cur.FECHA_INICIO;
+        vcargos.info_historial_cargos(INDICE).FECHA_FIN := v_cur.FECHA_FIN;
+        INDICE := INDICE + 1;
+    END LOOP;
+    select count(NOMBRE_CARGO) into VCARGOS.v_status_table from HISTORIAL_CARGOS;
 END;
 /
 
@@ -69,18 +64,29 @@ DECLARE
     v_rfecha NUMBER;
     contador NUMBER:=0;
 BEGIN
-    FOR v_cur in vcargos.info_historial_cargos.FIRST.. vcargos.info_historial_cargos.LAST LOOP
-        select comprobar_fecha_cargos(:NEW.FECHA_INICIO,INDICE2,:NEW.DNI,:NEW.CODCOMUNIDAD) into v_rfecha from dual;
-        IF v_rfecha = 1 THEN
-            contador := contador + 1;
-        END IF; 
-        IF contador = 1 THEN
-            RAISE_APPLICATION_ERROR(-20002, 'Los propietarios solo pueden ocupar un cargo en la misma comunidad');
-	    END IF;
-        INDICE2 := INDICE2 + 1;
-    END LOOP;
-    contador := 0;
-    vcargos.info_historial_cargos.DELETE;
+    IF VCARGOS.v_status_table > 0 THEN
+        FOR v_cur in vcargos.info_historial_cargos.FIRST.. vcargos.info_historial_cargos.LAST LOOP
+            select comprobar_fecha_cargos(:NEW.FECHA_INICIO,INDICE2,:NEW.DNI,:NEW.CODCOMUNIDAD) into v_rfecha from dual;
+            validacion_cargos(INDICE2,v_rfecha,contador);
+        END LOOP;
+        contador := 0;
+        vcargos.info_historial_cargos.DELETE;
+    end if;
+END;
+/
+
+--Procedimiento validación cargos
+
+create or replace procedure validacion_cargos(p_indice2 in out NUMBER, p_rfecha in out NUMBER, p_contador in out NUMBER)
+IS
+BEGIN
+    IF p_rfecha = 1 THEN
+        p_contador := p_contador + 1;
+    END IF; 
+    IF p_contador = 1 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Los propietarios solo pueden ocupar un cargo en la misma comunidad');
+	END IF;
+    p_indice2 := p_indice2 + 1;
 END;
 /
 
@@ -101,42 +107,6 @@ TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'));
 
 update HISTORIAL_CARGOS set NOMBRE_CARGO = 'Vicepresidente',CODCOMUNIDAD = 'AAAA1' 
 where DNI='49027387N' and CODCOMUNIDAD='AAAA2' and FECHA_FIN=TO_DATE('2019/01/15','YYYY/MM/DD');
-
-INSERT ALL
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Presidente','AAAA1','49027387N',TO_DATE('2016/01/15','YYYY/MM/DD'),TO_DATE('2017/01/15','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vicepresidente','AAAA1','50765614Z',TO_DATE('2016/01/15','YYYY/MM/DD'),TO_DATE('2017/01/15','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vocal','AAAA1','10880946Z',TO_DATE('2016/01/15','YYYY/MM/DD'),TO_DATE('2017/01/15','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vocal','AAAA1','23293294K',TO_DATE('2016/01/15','YYYY/MM/DD'),TO_DATE('2017/01/15','YYYY/MM/DD'))
-
-    INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Presidente','AAAA2','09291497A',TO_DATE('2016/01/05','YYYY/MM/DD'),TO_DATE('2017/01/05','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vicepresidente','AAAA2','52349896X',TO_DATE('2016/01/05','YYYY/MM/DD'),TO_DATE('2017/01/05','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vocal','AAAA2','X4945396M',TO_DATE('2016/01/05','YYYY/MM/DD'),TO_DATE('2017/01/05','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vocal','AAAA2','11830895V',TO_DATE('2016/01/05','YYYY/MM/DD'),TO_DATE('2017/01/05','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Presidente','AAAA3','71441529X',TO_DATE('2016/01/12','YYYY/MM/DD'),TO_DATE('2017/01/12','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vicepresidente','AAAA3','53042369E',TO_DATE('2016/01/12','YYYY/MM/DD'),TO_DATE('2017/01/12','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vocal','AAAA3','71128255L',TO_DATE('2016/01/12','YYYY/MM/DD'),TO_DATE('2017/01/12','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vocal','AAAA3','50863298V',TO_DATE('2016/01/12','YYYY/MM/DD'),TO_DATE('2017/01/12','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Presidente','AAAA1','49027387N',TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('vocal','AAAA3','49027387N',TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Vicepresidente','AAAA1','49027387N',TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'))
-
-	INTO HISTORIAL_CARGOS (NOMBRE_CARGO,CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN) VALUES ('Presidente','AAAA3','49027387N',TO_DATE('2018/01/15','YYYY/MM/DD'),TO_DATE('2019/01/15','YYYY/MM/DD'))
-
-SELECT * FROM dual;
 
 delete from HISTORIAL_CARGOS where FECHA_INICIO = TO_DATE('2018/01/15','YYYY/MM/DD');
 
@@ -161,20 +131,13 @@ BEGIN
     vcargos.info_historial_cargos.DELETE;
 END;
 
-BEGIN
-if (select COUNT(NOMBRE_CARGO) from VCARGOS.info_historial_cargos) = 0 THEN
-    dbms_output.put_line('Funciona');
-elsif (select COUNT(NOMBRE_CARGO) from VCARGOS.info_historial_cargos) > 0 THEN
-    dbms_output.put_line('Fallo');
-end if;
-END;
-
 DECLARE
-    cursor info_cargos IS select CODCOMUNIDAD,DNI,FECHA_INICIO,FECHA_FIN from HISTORIAL_CARGOS;
-BEGIN
-    IF info_cargos%found THEN
+    v_prueba NUMBER;
+BEGIN 
+    select count(NOMBRE_CARGO) into v_prueba from HISTORIAL_CARGOS;
+    IF v_prueba > 0 THEN
         dbms_output.put_line('1');
-    elsif info_cargos%notfound THEN
-        dbms_output.put_line('1');
+    ELSIF v_prueba = 0 THEN
+        dbms_output.put_line('0');
     end if;
 END;
