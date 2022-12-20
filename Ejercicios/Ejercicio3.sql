fceaa2083efb53c0ca651265a89addf965611741
Ivan <92149012+LainWireless@users.noreply.github.com>
2022-12-20 21:15:35 +0100

    Ejercicio 3 Terminado(Funciona)

--- Ejercicios/Ejercicio3.sql
+++ Ejercicios/Ejercicio3.sql
@@ -67,3 +67,80 @@ BEGIN
   asignar_honorarios_oficinas;
 END;
 /
+
+
+-- Función que comprueba que el valor introducido en honorarios anuales es correcto.
+CREATE OR REPLACE FUNCTION comprobar_honorarios_anuales(p_codcomunidad in VARCHAR2)
+RETURN NUMBER
+IS
+  v_honorarios_anuales NUMBER;
+  v_num_propiedades NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_num_propiedades FROM propiedades WHERE codcomunidad = p_codcomunidad;
+  IF v_num_propiedades BETWEEN 1 AND 5 THEN
+    v_honorarios_anuales:= 600;
+  ELSIF v_num_propiedades BETWEEN 6 AND 10 THEN
+    v_honorarios_anuales:= 1000;
+  ELSIF v_num_propiedades BETWEEN 11 AND 20 THEN
+    v_honorarios_anuales:= 1800;
+  ELSIF v_num_propiedades > 20 THEN
+    v_honorarios_anuales:= 2500;
+  END IF;
+  RETURN v_honorarios_anuales;
+END;
+/
+
+
+
+-- Función que comprueba que el valor introducido en honorarios anuales es correcto dependiendo del número de locales.
+CREATE OR REPLACE FUNCTION comprobar_honorarios_locales(p_codcomunidad in VARCHAR2)
+RETURN NUMBER
+IS
+  v_honorarios_locales NUMBER;
+  v_num_locales NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_num_locales FROM locales WHERE codcomunidad = p_codcomunidad;
+  IF v_num_locales > 0 THEN
+    v_honorarios_locales:= 1.2;
+  END IF;
+  RETURN v_honorarios_locales;
+END;
+/
+
+
+
+-- Función que comprueba que el valor introducido en honorarios anuales es correcto dependiendo del número de oficinas.
+CREATE OR REPLACE FUNCTION comprobar_honorarios_oficinas(p_codcomunidad in VARCHAR2)
+RETURN NUMBER
+IS
+  v_honorarios_oficinas NUMBER;
+  v_num_oficinas NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_num_oficinas FROM oficinas WHERE codcomunidad = p_codcomunidad;
+  IF v_num_oficinas > 0 THEN
+    v_honorarios_oficinas:= 1.1;
+  END IF;
+  RETURN v_honorarios_oficinas;
+END;
+/
+
+
+-- Trigger que compruebe que el valor introducido en honorarios anuales es correcto.
+-- Utiliza las funciones comprobar_honorarios, comprobar_honorarios_locales y comprobar_honorarios_oficinas.
+CREATE OR REPLACE TRIGGER t_comprobar_honorarios
+BEFORE INSERT OR UPDATE ON contratos_de_mandato
+FOR EACH ROW
+DECLARE
+  v_honorarios_anuales NUMBER;
+  v_honorarios_locales NUMBER;
+  v_honorarios_oficinas NUMBER;
+BEGIN
+  v_honorarios_anuales:= comprobar_honorarios_anuales(:new.codcomunidad);
+  v_honorarios_locales:= comprobar_honorarios_locales(:new.codcomunidad);
+  v_honorarios_oficinas:= comprobar_honorarios_oficinas(:new.codcomunidad);
+  IF :new.honorarios_anuales <> v_honorarios_anuales * v_honorarios_locales * v_honorarios_oficinas THEN
+    RAISE_APPLICATION_ERROR(-20001, 'El valor de honorarios anuales no es correcto.');
+  END IF;
+END;
+/
+
