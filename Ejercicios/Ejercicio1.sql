5eda7e8d0d982379aa4ce0af5cc4e605ca822cdb
davidrgfoss <davidrg@davidrgfoss.com>
2022-12-21 19:45:53 +0100

    Correcciones en el ejercicio1

--- Ejercicios/Ejercicio1.sql
+++ Ejercicios/Ejercicio1.sql
@@ -1,58 +1,58 @@
-CREATE OR REPLACE function ejercicio1 (p_codcomunidad IN comunidades.codcomunidad%type, p_codpropiedad IN propiedades.codpropiedad%type)
-RETURN NUMBER
-IS
-  p_resultado NUMBER;
-  No_comunidad EXCEPTION;
-  No_propiedad EXCEPTION;
-  No_comercial EXCEPTION;
-BEGIN
+create or replace function ejercicio1 (p_codcomunidad comunidades.codcomunidad%type, p_codpropiedad propiedades.codpropiedad%type)
+return number
+is
+  p_resultado number;
+  No_comunidad exception;
+  No_propiedad exception;
+  No_comercial exception;
+begin
   -- Comprobar si existe la comunidad
-  SELECT COUNT(*) INTO p_resultado FROM comunidades WHERE codcomunidad = p_codcomunidad;
-  IF p_resultado = 0 THEN
+  select count(*) into p_resultado from comunidades where codcomunidad = p_codcomunidad;
+  if p_resultado = 0 then
    -- RAISE_APPLICATION_ERROR(-20001, 'La comunidad introducida no existe');
-    RAISE No_comunidad;
-  END IF;
+    raise No_comunidad;
+  end if;
   -- Comprobar si existe la propiedad en esa comunidad
-  SELECT COUNT(*) INTO p_resultado FROM propiedades WHERE codcomunidad = p_codcomunidad AND codpropiedad = p_codpropiedad;
-  IF p_resultado = 0 THEN
+  select count(*) into p_resultado from propiedades where codcomunidad = p_codcomunidad and codpropiedad = p_codpropiedad;
+  if p_resultado = 0 then
    -- RAISE_APPLICATION_ERROR(-20002, 'La propiedad introducida no existe en esa comunidad');
-    RAISE No_propiedad;
-  END IF;
+    raise No_propiedad;
+  end if;
   -- Comprobar si la propiedad es un local comercial
-  SELECT COUNT(*) into p_resultado from locales WHERE codpropiedad = p_codpropiedad;
-  IF p_resultado = 0 THEN
+  select count(*) into p_resultado from locales where codpropiedad = p_codpropiedad;
+  if p_resultado = 0 then
    -- RAISE_APPLICATION_ERROR(-20003, 'La propiedad introducida no es comercial');
-    RAISE No_comercial;
-  END IF;
+    raise No_comercial;
+  end if;
   -- Obtener el estado del local
-  SELECT COUNT(*) INTO p_resultado FROM horarios WHERE codpropiedad = p_codpropiedad AND SYSDATE BETWEEN hora_apertura AND hora_cierre;
+  select count(*) into p_resultado from horarios where codpropiedad = p_codpropiedad and sysdate between hora_apertura and hora_cierre;
   -- Devolver el resultado
-  RETURN p_resultado;
-EXCEPTION
-  WHEN No_comunidad then
-    DBMS_OUTPUT.PUT_LINE('La comunidad introducida no existe');
-    RAISE;
-  WHEN No_propiedad then
-    DBMS_OUTPUT.PUT_LINE('La propiedad introducida no existe en esa comunidad');
-    RAISE;
-  WHEN No_comercial then
-    DBMS_OUTPUT.PUT_LINE('La propiedad introducida no es comercial');
-    RAISE;
-END ejercicio1;
+  return p_resultado;
+exception
+  when No_comunidad then
+    dbms_output.put_line('La comunidad introducida no existe');
+    raise;
+  when No_propiedad then
+    dbms_output.put_line('La propiedad introducida no existe en esa comunidad');
+    raise;
+  when No_comercial then
+    dbms_output.put_line('La propiedad introducida no es comercial');
+    raise;
+end ejercicio1;
 /
 
 
-CREATE OR REPLACE PROCEDURE ejercicio1_main (p_codcomunidad IN comunidades.codcomunidad%type, p_codpropiedad IN propiedades.codpropiedad%type)
-IS
-  v_estado NUMBER;
-BEGIN
+create or replace procedure ejercicio1_main (p_codcomunidad comunidades.codcomunidad%type, p_codpropiedad propiedades.codpropiedad%type)
+is
+  v_estado number;
+begin
   v_estado:=ejercicio1(p_codcomunidad, p_codpropiedad);
-  IF v_estado = 1 THEN
-    DBMS_OUTPUT.PUT_LINE('Estado del local: Abierto');
-  ELSE
-    DBMS_OUTPUT.PUT_LINE('Estado del local: Cerrado');
-  END IF;
-END ejercicio1_main;
+  if v_estado = 1 then
+    dbms_output.put_line('Estado del local: Abierto.');
+  else
+    dbms_output.put_line('Estado del local: Cerrado.');
+  end if;
+end ejercicio1_main;
 /
 
 
