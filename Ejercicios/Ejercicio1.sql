193c65894abf7be928866b5975381941468d22c3
davidrgfoss <davidrg@davidrgfoss.com>
2022-12-22 00:27:53 +0100

    Ejercicio 1 corregido

--- Ejercicios/Ejercicio1.sql
+++ Ejercicios/Ejercicio1.sql
@@ -2,7 +2,7 @@ create or replace procedure ejercicio1_main (p_codcomunidad comunidades.codcomun
 is
   v_estado number;
 begin
-  v_estado:=ejercicio1(p_codcomunidad, p_codpropiedad);
+  v_estado:=ejercicio11(p_codcomunidad, p_codpropiedad);
   if v_estado = 1 then
     dbms_output.put_line('Estado del local: Abierto.');
   else
@@ -14,18 +14,18 @@ exception
 end ejercicio1_main;
 /
 
-create or replace function ejercicio1 (p_codcomunidad comunidades.codcomunidad%type, p_codpropiedad propiedades.codpropiedad%type)
+create or replace function ejercicio11 (p_codcomunidad comunidades.codcomunidad%type, p_codpropiedad propiedades.codpropiedad%type)
 return number
 is
   p_resultado number;
 begin
-  comprobar_excepciones_ej1(p_codcomunidad, p_codpropiedad);
+  comprobar_excepciones_ej1(p_codcomunidad, p_codpropiedad, p_resultado);
   select count(*) into p_resultado from horarios_apertura where codcomunidad=p_codcomunidad and codpropiedad=p_codpropiedad and to_char(LOCALTIMESTAMP, 'HH:MI:SS') between to_char(hora_apertura, 'HH:MI:SS') and to_char(hora_cierre, 'HH:MI:SS');
   return p_resultado;
-end ejercicio1;
+end ejercicio11;
 /
 
-create or replace procedure comprobar_excepciones_ej1 (p_codcomunidad comunidades.codcomunidad%type, p_codpropiedad propiedades.codpropiedad%type)
+create or replace procedure comprobar_excepciones_ej1 (p_codcomunidad comunidades.codcomunidad%type, p_codpropiedad propiedades.codpropiedad%type, p_resultado in out number)
 is
   No_comunidad exception;
   No_propiedad exception;
